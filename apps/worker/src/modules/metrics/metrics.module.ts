82b03db8bb9e90d77ede430c5f13db2b64f7e41d
Scott Caesar <scott.caesar@gmail.com>
2026-03-04 11:39:35 -0500

    feat: add stage 14 observability baseline

---
+++ apps/worker/src/modules/metrics/metrics.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+
+import { MetricsController } from './metrics.controller.js';
+
+@Module({
+  controllers: [MetricsController],
+})
+export class MetricsModule {}
